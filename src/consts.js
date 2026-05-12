0eb7ac9ba4a58f1417c00559f98ff5c7e2c1beb5
SHU <shu@private.ip.shu.edu.tw>
2026-05-12 19:52:10 +0800

    init: 環球新聞 Astro blog

---
+++ src/consts.js
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = '環球新聞';
+export const SITE_DESCRIPTION = '最新國際與本地新聞，即時掌握全球動態。';
